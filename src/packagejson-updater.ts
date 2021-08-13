301faf49fedba12a062a311e2db0287ac502bee7
Vemund Gaukstad <tjololo@users.noreply.github.com>
2021-08-13 17:14:36 +0200

    bugfix: update failed on missing object in json
    update failed if devDependencies or dependencies object was missing

--- src/packagejson-updater.ts
+++ src/packagejson-updater.ts
@@ -14,15 +14,14 @@ export class PackageJsonUpdater {
         const packageJsonObject = JSON.parse(packageJsonContent)
         for (const outdatedPackage of outdated) {
             core.info(`${outdatedPackage.name} is ${outdatedPackage.current} wanting ${outdatedPackage.wanted}`)
-            if (packageJsonObject.dependencies[outdatedPackage.name]) {
+            if (packageJsonObject.dependencies && packageJsonObject.dependencies[outdatedPackage.name]) {
                 const orig = packageJsonObject.dependencies[outdatedPackage.name]
                 if (this.shouldUpdatePackageJson(orig)) {
                     packageJsonObject.dependencies[outdatedPackage.name] = `${orig[0]}${outdatedPackage.wanted}`
                 } else {
                     core.info(`Skippoing update of version for ${outdatedPackage.name} as update of version string ${orig} has no affect`)
                 }
-            }
-            if (packageJsonObject.devDependencies[outdatedPackage.name]) {
+            } else if (packageJsonObject.devDependencies && packageJsonObject.devDependencies[outdatedPackage.name]) {
                 const orig = packageJsonObject.devDependencies[outdatedPackage.name]
                 if (this.shouldUpdatePackageJson(orig)) {
                     packageJsonObject.devDependencies[outdatedPackage.name] = `${orig[0]}${outdatedPackage.wanted}`
