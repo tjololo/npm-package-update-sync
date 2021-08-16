2cd607c789610e00ba6a45419d38be30c3e20347
Vemund Gaukstad <tjololo@users.noreply.github.com>
2021-08-16 12:37:49 +0200

    removed unused import

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { readFileSync, statSync } from 'fs'
+import { statSync } from 'fs'
 import * as path from 'path'
 import { NpmCommandManager } from './npm-command-manager'
 import { getAllProjects } from './npm-project-locator'
